981edc1ec3614699aa354604301a6eeeb927054f
Niall O'Higgins <niallo@frozenridge.co>
2013-07-11 17:01:03 -0600

    add dnode client support refactor `edit` command

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 #!/usr/bin/env node
 
 var data = require('level-userdb')
+var dnodeClient = require('level-userdb-dnode').client
 var editor = require('editor')
 var fs = require('fs')
 var mktemp = require('mktemp')
 var ui = require('optimist')
-  .usage('Usage: $0 -d [database] -e [email] -p [password] <COMMAND>')
+  .usage('Usage: $0 -d [database] -r [server:port] -e [email] -p [password] <COMMAND>')
   .alias('d', 'database')
   .describe('d', 'Path to database location')
   .alias('e', 'email')
@@ -14,6 +15,8 @@ var ui = require('optimist')
   .describe('n', 'new email address')
   .alias('p', 'password')
   .describe('p', 'password')
+  .alias('r', 'remote')
+
 
 var oh = ui.help()
 ui.help = help
@@ -21,7 +24,7 @@ ui.help = help
 var commands = {
   create: {desc:"Create a new user with email & password"},
   delete: {desc:"Delete a user by email"},
-  edit: {desc:"Edit a user's data with $EDITOR"},
+  edit: {desc:"Edit a user's data field with $EDITOR"},
   email: {desc:"Update a user's email address"},
   list: {desc:"List all users"},
   password: {desc:"Update a user's password by email"},
@@ -52,30 +55,68 @@ function bail() {
   process.exit(1)
 }
 
+if (!argv.r && !argv.d) {
+  console.log('Error: You must specify a database via -d (filesystem) or -r (remote)')
+  ui.showHelp()
+  process.exit(1)
+}
+
+if (argv.r && argv.d) {
+  console.log('Error: You cannot specify BOTH a database via -d (filesystem) and -r (remote)')
+  ui.showHelp()
+  process.exit(1)
+}
+
+
 if (argv._.length < 1) bail()
 
 argv._.forEach(function(cmd) {
   if (Object.keys(commands).indexOf(cmd) === -1) {
     bail()
   }
-  var db = data(argv.database)
+  if (argv.database) {
+    handleCmd(cmd, data(argv.database), function() {})
+  }
+  if (argv.remote) {
+    var host = argv.remote.split(':')[0]
+    var port = 9998
+    try {
+      port = parseInt(argv.remote.split(':')[1])
+    } catch(e) {
+    }
+    console.log("connecting to %s:%d", host, port)
+    dnodeClient({host:host, port:port}, function(r, c) {
+      console.log("connected to %s:%d", host, port)
+      handleCmd(cmd, r, function() {
+        c.end()
+        process.exit(0)
+      })
+    })
+
+  }
+})
+
+function handleCmd(cmd, db, cb) {
   if (cmd === 'create') {
     console.log("Creating user")
     db.addUser(argv.email, argv.password, function(err) {
       if (err) throw err
       console.log("ok")
+      cb(null)
     })
   } else if (cmd === 'retrieve') {
     db.findUser(argv.email, function(err, userObj) {
       if (err) throw err
       console.log("User email: %s", argv.email)
       console.log("==============================")
-      console.log("Created:\t %s", userObj.createdTimestamp)
-      console.log("Modified:\t %s", userObj.modifiedTimestamp)
+      console.log("Created:\t %s", userObj.createdDate)
+      console.log("Modified:\t %s", userObj.modifiedDate)
       console.log("Password hash:\t %s", userObj.password)
       console.log("Arbitrary data:\t %s", JSON.stringify(userObj.data, null, '\t'))
+      cb(null)
     })
   } else if (cmd === 'verify') {
+    console.log("verify")
     db.checkPassword(argv.email, argv.password, function(err, res) {
       console.log("User email: %s", argv.email)
       console.log("==============================")
@@ -85,6 +126,7 @@ argv._.forEach(function(cmd) {
 
       }
       console.log("Good password")
+      cb(null)
     })
   } else if (cmd === 'delete') {
     db.deleteUser(argv.email, function(err, res) {
@@ -92,6 +134,7 @@ argv._.forEach(function(cmd) {
       console.log("User email: %s", argv.email)
       console.log("==============================")
       console.log("Deleted.")
+      cb(null)
     })
   } else if (cmd === 'email') {
     if (!argv.n) {
@@ -103,27 +146,61 @@ argv._.forEach(function(cmd) {
       console.log("User email: %s", argv.email)
       console.log("==============================")
       console.log("Email updated to: %s", argv.n)
+      cb(null)
     })
   } else if (cmd === 'list') {
     db.printAllUsers()
+    cb(null)
   } else if (cmd === 'edit') {
-    db.findUser(argv.email, function(err, user) {
-      mktemp.createFile('dbajs-XXXXXX', function(err, path) {
-        if (err) throw err
-          fs.writeFile(path, JSON.stringify(user.data, null, '\t'), function(err) {
-            if (err) throw err
-            editor(path, function(code, sig) {
-              fs.readFile(path, function(err, data) {
-                if (err) throw err
-                db.modifyUser(argv.email, JSON.parse(data), function(err) {
-                  fs.unlink(path, function() {})
-                  console.log("ok")
-                })
-              })
-            })
-        })
-      })
-    })
-  }
-})
+    var user
 
+    function find() {
+      db.findUser(argv.email, mktempFile)
+    }
+
+    function mktempFile(err, u) {
+      if (err) {
+        console.log("Error: %s not found, exiting", argv.email)
+        process.exit(1)
+      }
+      user = u
+      mktemp.createFile('dbajs-XXXXXX', writeFile)
+    }
+
+    function writeFile(err, path) {
+      if (err) throw err
+      fs.writeFile(path, JSON.stringify(user.data, null, '\t'), editFile)
+    }
+
+    function editFile(err) {
+      if (err) throw err
+      editor(path, readEditedFile)
+    }
+
+    function readEditedFile(code, sig) {
+      fs.readFile(path, modifyUser)
+    }
+
+    function modifyUser(err, data) {
+      if (err) throw err
+
+      var d
+      try {
+        d= JSON.parse(data)
+      } catch(e) {
+        console.log("invalid JSON, cannot modify")
+        process.exit(1)
+      }
+      db.modifyUser(argv.email, d, cleanup)
+    }
+
+    function cleanup(err) {
+      fs.unlink(path, function() {})
+      console.log("ok")
+      cb(null)
+    }
+
+    find()
+
+  }
+}
